5e4b3200ef2e83c3ff0319ca5f4e010cf3fc99d5
Quvonchbek <147556696+IbragimovQuvonchbek@users.noreply.github.com>
2023-11-24 18:03:58 +0500

    fixed bugs

--- netflix/netflix.py
+++ netflix/netflix.py
@@ -1,39 +1,39 @@
 # Admin-login:ibragimov10-password:abc123
 from functions import signup, login, addMovie, showMovieList, likeMovie
 
-option = int(input("Sing up | Log in (0|1): "))
-currentUser = 0
-isAdmin = False
-if option == 0:
-    print("=========Sign up=============")
-    register = signup()
-    print("successfully registred" if register != 0 else "username already exists")
-if option == 1 or option == 0:
-    print("=========Log in==============")
-    currentUser, isAdmin = login()
-    print("success" if currentUser != 0 else "incorrect username or password")
-elif option != 0 != 1:
-    print("invalid option")
-
-while True and currentUser != 0:
-    optionForAll = 0
-    if isAdmin:
-        optionForAll = int(input("Exit | Watch movies |  Add movie (0|1|2): "))
-    else:
-        optionForAll = int(input("Exit | Watch movies (0|1): "))
-
-    if optionForAll == 0:
+while True:
+    option = int(input("Exit | Sing up | Log in (0|1|2): "))
+    currentUser = 0
+    isAdmin = False
+    if option == 1:
+        print("=========Sign up=============")
+        register = signup()
+        print("successfully registred" if register != 0 else "username already exists")
+    if option == 1 or option == 2:
+        print("=========Log in==============")
+        currentUser, isAdmin = login()
+        print("success" if currentUser != 0 else "incorrect username or password")
+    elif option == 0:
         break
-    elif optionForAll == 1:
-        showMovieList()
-        while True:
-            option2 = int(input("Exit | Like movies (0|1): "))
-            if option2 == 0:
-                break
-            elif option2 == 1:
-                movieId = int(input("Write movie id: "))
-                likeMovie(currentUser, movieId)
-    elif optionForAll == 2 and isAdmin:
-        addMovie()
-    else:
-        print("invalid option")
+    while True and currentUser != 0:
+        optionForAll = 0
+        if isAdmin:
+            optionForAll = int(input("Back | Watch movies |  Add movie (0|1|2): "))
+        else:
+            optionForAll = int(input("Back | Watch movies (0|1): "))
+
+        if optionForAll == 0:
+            break
+        elif optionForAll == 1:
+            showMovieList()
+            while True:
+                option2 = int(input("Back | Like movies (0|1): "))
+                if option2 == 0:
+                    break
+                elif option2 == 1:
+                    movieId = int(input("Write movie id: "))
+                    likeMovie(currentUser, movieId)
+        elif optionForAll == 2 and isAdmin:
+            addMovie()
+        else:
+            print("invalid option")
